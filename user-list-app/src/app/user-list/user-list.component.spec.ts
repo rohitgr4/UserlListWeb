19a614633cd9764ceff8334164aad0ddba3ba5ac
rohitgr4 <rohit4grover@gmail.com>
2024-05-24 18:39:37 +0530

    Test case

--- user-list-app/src/app/user-list/user-list.component.spec.ts
+++ user-list-app/src/app/user-list/user-list.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService, User } from '../user.service';
+import { of } from 'rxjs';
+
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
+  const mockUsers = [
+    { name: 'Albert Miguel', email: 'albert.miguel@example.com' },
+    { name: 'Jona', email: 'jane.smith@example.com' },
+    { name: 'Michael Johnson', email: 'michael.johnson@example.com' },
+    { name: 'Emily Ross', email: 'emily.ross@example.com' }
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserListComponent]
+      declarations: [ UserListComponent ],      
+      providers: [ UserService ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService); 
+    fixture.detectChanges();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should fetch and display users', () => {
+    spyOn(userService, 'getUsers').and.returnValue(of(mockUsers)); 
+
+    component.ngOnInit(); 
+
+    expect(userService.getUsers).toHaveBeenCalled(); 
+    expect(component.users).toEqual(mockUsers); 
   });
 });
